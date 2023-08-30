86a390775c8b63fc512a8b09e462bf450760bd11
Cesar Silva <cesarfilhoslv@gmail.com>
2023-08-30 10:07:19 -0300

    tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next"
 
 import { AuthLoginForm } from "@/components/auth-login-form"
 import { SchedulingForm } from "@/components/scheduling-form"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export const metadata: Metadata = {
   title: "Login",
@@ -9,12 +10,18 @@ export const metadata: Metadata = {
 
 export default function LoginPage() {
   return (
-    <div className="container flex h-screen w-screen flex-col items-center justify-center">
-      <div className="mx-auto flex w-full flex-col justify-center space-y-12 sm:w-[350px]">
-      <span className="text-center text-muted-foreground">Logo aqui</span>
-      {/* <AuthLoginForm /> */}
-      <SchedulingForm />
-      </div>
-    </div>
+      <Tabs defaultValue="login">
+        <div className="container flex h-screen w-screen flex-col items-center justify-center">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-12 sm:w-[350px]">
+            <TabsList className="absolute top-10 mx-auto translate-x-1/2">
+              <TabsTrigger value="login">Login</TabsTrigger>
+              <TabsTrigger value="schedulign">Schedulign</TabsTrigger>
+            </TabsList>
+          <span className="text-center text-muted-foreground">Logo aqui</span>
+            <TabsContent value="login"><AuthLoginForm /></TabsContent>
+            <TabsContent value="schedulign"> <SchedulingForm /></TabsContent>
+          </div>
+        </div>
+      </Tabs>
   )
 }
